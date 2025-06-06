d811988a79552d8376cd6eec6eea6f9334e2a534
Chad Gothelf <chad.gothelf@uconn.edu>
2025-06-06 18:03:49 -0400

    changes

--- environment.py
+++ environment.py
@@ -142,7 +142,7 @@ def possible_actions(history, I):
         return {0:0, 1:0, 2:0, 3:0, 4:0, 5:0}
 
 
-#TODO ZACHY
+
 def get_infoset(history, curr_player):
     """
     :param history: Example of tuple:
